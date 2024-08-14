dd2a0efdf61f13bb5393dd466728db7eb838caef
Ivaylo Lalov <ivolalov13@gmail.com>
2024-08-14 15:31:26 +0300

    add search page and functionality

---
+++ client/src/components/search/Search.tsx
@@ -0,0 +1,90 @@
+// export default function Search() {
+//   return (
+//     <>
+//       <div className="search">
+//         <input type="text" />
+//         <button>Search</button>
+//       </div>
+//     </>
+//   );
+// }
+
+import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { WorkoutType } from "../../types/WorkoutType";
+
+import * as workoutService from "../../services/workoutService";
+import WorkListItem from "../workoutList/workoutListItem/WorkoutListItem";
+
+export default function Search() {
+  // Sample data items
+  //   const items: WorkoutType[] = [
+  //    { _ownerId: '',
+  //     type: '',
+  //     duration: '',
+  //     imageUrl: '',
+  //     difficulty: '',
+  //     description: '',
+  //     _createdOn: 0,
+  //     _id: ''}
+  //   ];
+
+  const [filteredItems, setFilteredItems] = useState<WorkoutType[]>([]);
+
+  return (
+    <div className="search-wrapper">
+      <h3 className="search-title">Search type of workouts</h3>
+      <Formik
+        initialValues={{ search: "" }}
+        validate={(values) => {
+          const errors: { search?: string } = {};
+          if (!values.search) {
+            errors.search = "Required";
+          }
+          return errors;
+        }}
+        onSubmit={(values) => {
+          const searchValue = values.search.toLowerCase();
+
+          workoutService
+            .getAllBySearch(searchValue)
+            .then((res) => setFilteredItems(res));
+
+          console.log(filteredItems);
+        }}
+      >
+        {() => (
+          <Form>
+            <div>
+              {/* <label htmlFor="search">Search:</label> */}
+              <Field className="search-input" name="search" type="text" />
+              <ErrorMessage
+                name="search"
+                component="div"
+                className="error-validation"
+              />
+            </div>
+            <button
+              className="btn btn-primary btn-block btn-large"
+              type="submit"
+            >
+              Search
+            </button>
+          </Form>
+        )}
+      </Formik>
+
+      <div className="search-items-div">
+        {filteredItems.length > 0 ? (
+          <ul className="search-ul">
+            {filteredItems.map((workout) => (
+              <WorkListItem key={workout._id} {...workout} />
+            ))}
+          </ul>
+        ) : (
+          <p className="no-match-p">There is no match...</p>
+        )}
+      </div>
+    </div>
+  );
+}
--- client/src/components/workoutList/workoutListItem/WorkoutListItem.tsx
+++ client/src/components/workoutList/workoutListItem/WorkoutListItem.tsx
@@ -15,9 +15,9 @@ export default function WorkListItem({
           <img src={imageUrl} />
         </div>
         <div className="card-text">
-          <p className="card-meal-type">{type}</p>
+          <p className="card-meal-type">{type.toUpperCase()}</p>
           <h2 className="card-title">{duration} minutes</h2>
-          <p className="card-body">{difficulty}</p>
+          <p className="card-body">{difficulty.toLowerCase()}</p>
         </div>
         <button className="card-price">
           <Link className="details-description" to={`/workouts/${_id}`}>
